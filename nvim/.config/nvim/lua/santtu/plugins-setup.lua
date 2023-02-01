f0695014afd75756643509844be4cc7803144775
ST <santtu.tuovinen2@gmail.com>
2023-02-01 18:38:22 +0200

    Remove lspsaga plugin

--- nvim/.config/nvim/lua/santtu/plugins-setup.lua
+++ nvim/.config/nvim/lua/santtu/plugins-setup.lua
@@ -73,7 +73,6 @@ return packer.startup(function(use)
 	-- configuring lsp servers
 	use("neovim/nvim-lspconfig")
 	use("hrsh7th/cmp-nvim-lsp")
-	use({ "glepnir/lspsaga.nvim", branch = "main" })
 	use("jose-elias-alvarez/typescript.nvim")
 	use("onsails/lspkind.nvim")
 
