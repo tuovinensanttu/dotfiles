e2870b58aee50fffdc0117e06b4f87c154e6ec23
Santtu Tuovinen <santtu@Santtus-MBP.wlan>
2024-03-01 12:13:46 +0200

    Turn background to false to use colorscheme background

--- dot_config/nvim/lua/plugins/colorscheme.lua
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -2,9 +2,7 @@ return {
   "craftzdog/solarized-osaka.nvim",
   lazy = true,
   priority = 1000,
-  opts = function()
-    return {
-      transparent = true,
-    }
-  end,
+  opts = {
+    transparent = false,
+  },
 }
