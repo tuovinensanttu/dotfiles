a41891b6d8c1776923a7575a7970f02be6a34376
Santtu Tuovinen <santtu.tuovinen@crowdcollective.com>
2024-04-22 12:14:23 +0300

    Update packages

--- dot_config/nvim/lua/plugins/obsidian.lua
+++ dot_config/nvim/lua/plugins/obsidian.lua
@@ -22,6 +22,10 @@ return {
         name = "personal",
         path = "~/vaults/personal",
       },
+      {
+        name = "work",
+        path = "~/vaults/work",
+      },
     },
 
     -- see below for full list of options 👇
